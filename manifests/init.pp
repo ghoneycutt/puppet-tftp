beed0fd64f395c4d7d10bc298ce8e89d258a4cf3
Johan Huysmans <johan@desk-jhu.localdomain>
2012-10-11 10:25:32 +0200

    Remove base_alias argument, this functionality does not belong inside the module. Fix errors found by puppet-lint.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,52 +1,32 @@
-# Class: tftp
-#
-# This module manages tftp
-#
-# Requires: class xinetd
-#
-# Sample Usage: include tftp
-#
-class tftp ($base = "/usr/local/tftpboot",
-            $base_alias = "",
+class tftp ($base = '/tftpboot',
             $refuse = []) {
 
     include xinetd
 
-    package { "tftp-server":
-        require => Class["xinetd"],
+    package { 'tftp-server':
+        require => Class['xinetd'],
     } # package
 
-    file { "$base":
-        ensure => directory;
-    } # file
-
-    if $base_alias {
-      file { "$base_alias":
-          ensure => link,
-          target => "$base",
-          force  => true;
-      }
-    }
-
     if $refuse {
-      $refuse_args = inline_template("<%= refuse.map{ |ref| '-r ' + ref + ' ' } %>")
+      $refuse_args = inline_template('<%= refuse.map{ |ref| "-r " + ref + " " } %>')
     }
 
-    $server_args = "$refuse_args-s $base"
+    $server_args = "${refuse_args}-s ${base}"
 
-    xinetd::service {"tftp":
-        port        => "69",
-        server      => "/usr/sbin/in.tftpd",
+    xinetd::service {'tftp':
+        port        => '69',
+        server      => '/usr/sbin/in.tftpd',
         server_args => $server_args,
-        socket_type => "dgram",
-        protocol    => "udp",
-        cps         => "100 2",
-        flags       => "IPv4",
-        per_source  => "11",
+        socket_type => 'dgram',
+        protocol    => 'udp',
+        cps         => '100 2',
+        flags       => 'IPv4',
+        per_source  => '11',
     } # xinetd::service
 
-    service { "tftp":
+    service { 'tftp':
         enable    => true,
-        require   => [ Package["tftp-server"], Xinetd::Service["tftp"] ],
+        require   => [ Package['tftp-server'], Xinetd::Service['tftp'] ],
     } # service
+
 } # class tftp
